96ff3ae4de6a86fc8a7df2debbf1fbe26837f06d
Jhon Avila <jhonavila@Jhons-MacBook-Pro.local>
2020-11-05 13:26:18 +0200

    added custom attributes script

--- js/init.js
+++ js/init.js
@@ -1,11 +1,22 @@
- var setAttributes = function(){
+ var userData = function(){
+     var result={};
+    fetch("https://ipapi.co/json/")
+    .then(response=>response.json())
+    .then((responseJson=>{
+        console.log(responseJson)
+        result=responseJson;
+    }))
+    return result;
+ }
+ 
+ var setAttributes = function(visitorData){
  sm.getApi({version: 'v1'}).then(function(salemove) {
-        salemove.updateInformation({ "customAttributes":{"State":"MA", "Quote Phase":"bind", "Quote Recall":"Yes", "Insurance Product":"home"}}).then(function() {
+        salemove.updateInformation({ "customAttributes":{"Country":visitorData['country'], "country_name":visitorData['country_name'], "Quote Recall":"Yes", "currency":visitorData['currency'],"languages":visitorData['languages']}}).then(function() {
             console.log("success");
         }).catch(function(error) {
             console.log("error");
         });
     });
 }
-setAttributes();
+setAttributes(userData());
 console.log("Init script loaded");
