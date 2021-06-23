122dd18536ba95b351634d91824db0eac7dca266
Jhon <jhon.avila@glia.com>
2021-06-23 17:05:05 +0300

    siteID for multiple sites using allowed origins

--- js/init.js
+++ js/init.js
@@ -127,3 +127,27 @@ var res = userData();
 
 
 console.log("Init script loaded");
+
+var installGlia = function(siteId, callback) {
+  var gliaIntegrationScriptUrl = 'https://api.glia.com/salemove_integration.js?site_id=' + siteId;
+  var scriptElement = document.createElement('script');
+
+  scriptElement.async = 1;
+  scriptElement.src = gliaIntegrationScriptUrl;
+  scriptElement.type = 'text/javascript';
+  if (typeof(callback) === 'function') {
+    scriptElement.addEventListener('load', callback);
+  }
+  
+  document.body.append(scriptElement);
+};
+
+/* 
+siteId is a UUID for your site that needs to be requested from your Client Success Manager 
+or via our Service Desk https://salemove.atlassian.net/servicedesk/customer/portal/1 
+*/
+var siteId = '2ef2e3a5-4593-4d9d-994e-f827243cc228'; 
+
+installGlia(siteId, function(){
+  // Any post-install configuration
+});
