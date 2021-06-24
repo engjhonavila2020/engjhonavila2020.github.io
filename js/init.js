4e7a30410ed8e6da13809acc46162a6cea25c14b
Jhon <jhon.avila@glia.com>
2021-06-24 15:01:13 +0300

    script location moved to main JS

--- js/init.js
+++ js/init.js
@@ -8,6 +8,36 @@
     }));
     
  };
+
+ var installGlia = function (siteId, callback) {
+  var gliaIntegrationScriptUrl = 'https://api.glia.com/salemove_integration.js?site_id=' + siteId;
+  var scriptElement = document.createElement('script');
+
+  scriptElement.async = 1;
+  scriptElement.src = gliaIntegrationScriptUrl;
+  scriptElement.type = 'text/javascript';
+  if (typeof (callback) === 'function') {
+    scriptElement.addEventListener('load', callback);
+  }
+
+  document.head.append(scriptElement);
+};
+
+/* 
+siteId is a UUID for your site that needs to be requested from your Client Success Manager 
+or via our Service Desk https://salemove.atlassian.net/servicedesk/customer/portal/1 
+*/
+var siteId = '2ef2e3a5-4593-4d9d-994e-f827243cc228';
+
+installGlia(siteId, function () {
+  // Any post-install configuration
+  console.log("siteId "+siteId);
+});
+
+
+
+
+
  
  var setAttributes = function(visitorData){
  sm.getApi({version: 'v1'}).then(function(salemove) {
